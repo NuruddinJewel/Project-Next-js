64f959f4265eb0a68085136a802cd16b5ffe0c9a
Jewel <m.76jewel@gmail.com>
2026-04-26 17:16:56 +0600

    Fetching Data

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,17 +1,27 @@
+import LeftSidebar from "@/components/Homepage/news/LeftSidebar";
+import RightSidebar from "@/components/Homepage/news/RightSidebar";
 import Image from "next/image";
+
+// async function getCategories() {
+//   const res = await fetch("https://openapi.programming-hero.com/api/news/categories");
+//   const data = await res.json();
+//   return data.data;
+// }
 
 async function getCategories() {
   const res = await fetch("https://openapi.programming-hero.com/api/news/categories");
+  if (!res.ok) throw new Error("Failed to fetch News data");
   const data = await res.json();
   return data.data;
 }
+
 
 export default async function Home() {
   const categories = await getCategories();
   console.log(categories.news_category, "categories");
   return <div className="container mx-auto grid grid-cols-12 gap-4 my-15">
     <div className="col-span-3">
-      <h2 className="font-bold text-lg">
+      {/* <h2 className="font-bold text-lg">
         All Categories
       </h2>
 
@@ -24,15 +34,43 @@ export default async function Home() {
 
           })
         }
-      </ul>
+      </ul> */}
+      <LeftSidebar categories={categories} activeId="" />
     </div>
     <div className="font-bold text-2xl bg-purple-100 col-span-6">
       All News
     </div>
-    <div className="font-bold text-2xl bg-yellow-100 col-span-3">
-      Social Icons
+    <div className="col-span-3">
+      <RightSidebar />
     </div>
   </div>
 
 
 }
+// import LeftSidebar from "@/components/Homepage/news/LeftSidebar";
+// import RightSidebar from "@/components/Homepage/news/RightSidebar";
+
+// async function getCategories() {
+//   const res = await fetch("https://openapi.programming-hero.com/api/news/categories");
+//   if (!res.ok) throw new Error("Failed to fetch categories");
+//   const data = await res.json();
+//   return data.data;
+// }
+
+// export default async function Home() {
+//   const categories = await getCategories();
+
+//   return (
+//     <div className="container mx-auto grid grid-cols-12 gap-4 my-15">
+//       <div className="col-span-3">
+//         <LeftSidebar categories={categories} activeId="" />
+//       </div>
+//       <div className="col-span-6">
+//         All News
+//       </div>
+//       <div className="col-span-3">
+//         <RightSidebar />
+//       </div>
+//     </div>
+//   );
+// }
