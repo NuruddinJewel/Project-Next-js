1dde13e3e37160681276dee58fa1eae0060a80d5
Jewel <m.76jewel@gmail.com>
2026-04-29 00:23:58 +0600

    Added Alert Message

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -23,6 +23,12 @@ const LoginPage = () => {
             callbackURL: "/",
         });
         console.log(res, error);
+        if (error) {
+            alert(error.message)
+        }
+        if (res) {
+            alert("Login successful")
+        }
     }
     // console.log(errors, "errors")
     // console.log(watch('email'))
