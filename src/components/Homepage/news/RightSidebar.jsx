7c145589664328fdd18e81abfa81c66b7a3b384b
Jewel <m.76jewel@gmail.com>
2026-04-28 19:02:28 +0600

    Google and Github Feature Updated

--- src/components/Homepage/news/RightSidebar.jsx
+++ src/components/Homepage/news/RightSidebar.jsx
@@ -1,12 +1,26 @@
+'use client'
+import { authClient } from '@/lib/auth-client';
 import React from 'react';
 import { FaGithub, FaGoogle } from "react-icons/fa";
 const RightSidebar = () => {
+    const handleGoogleSign = async () => {
+        const data = await authClient.signIn.social({
+            provider: "google",
+        });
+        console.log(data, "data");
+    }
+    const handleGithubSign = async () => {
+        const data = await authClient.signIn.social({
+            provider: "github",
+        });
+        console.log(data, "data");
+    }
     return (
         <div>
             <h2 className='font-bold text-lg mb-4'>Login with</h2>
             <div className='flex flex-col gap-2'>
-                <button className='btn border-blue-500 text-blue-500 '><FaGoogle />Login with Goolge</button>
-                <button className='btn'><FaGithub />Login with Github</button>
+                <button className='btn border-blue-500 text-blue-500' onClick={handleGoogleSign} ><FaGoogle />Login with Goolge</button>
+                <button className='btn' onClick={handleGithubSign}><FaGithub />Login with Github</button>
             </div>
         </div>
     );
